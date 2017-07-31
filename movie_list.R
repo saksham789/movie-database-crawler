fa21501792ed9eee31a1408e72ef4391eb94f202
saksham vikram <saksham.vikram12@gmail.com>
2017-07-31 22:21:23 +0530

    added movie crawler

---
+++ movie_list.R
@@ -0,0 +1,32 @@
+###
+install.packages("rvest")
+##
+p<-"http://www.bollywoodhungama.com/directory/movies-list/alphabet/@/page/#/"
+a<-LETTERS
+u<-c(NULL)
+for(i in 1:2){
+  b<-  gsub("@",a[i],p)
+  c<-gsub("#",2,b)
+  url<-(c)
+  webpage<-read_html(url)
+  rank_data<-html_nodes(webpage,"#movies > div > nav > a:nth-child(7)")
+  text<-html_text(rank_data)
+  x<-as.integer(text)
+  
+  for(j in 1:x){
+    f<-gsub("#",j,b)
+    url<-(f)
+    webpage<-read_html(url)
+    rank_data<-html_nodes(webpage,"#movies > div > ul")
+    text<-html_text(rank_data)
+    l<-grep("[a-zA-Z]",strsplit(text,"\n")[[1]],value = TRUE)
+    
+    for(i in 1:length(l)){u<-c(u,grep("[a-zA-Z]",strsplit(l,"\t")[[i]],value = TRUE))}
+    
+    v<-data.frame(u)
+    write.csv(v,"crawler.csv")
+  }
+  
+}
+
+
