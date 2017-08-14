9365926720b765776bf5a95f03de1e424ff490f3
reSHARMA <reshabhsh@gmail.com>
2017-08-14 08:52:45 +0530

    tweaked sleep parameters

--- movie_details.py
+++ movie_details.py
@@ -4,7 +4,7 @@ import urllib.parse
 import json
 from pprint import pprint
 import string
-f = open('movie_data.txt','w')
+#f = open('movie_data.txt','w')
 YEAR = 2000
 with open("movie.json",encoding='cp1252') as movie_list:
 	movie = json.load(movie_list)
@@ -18,16 +18,20 @@ for x in range(0,len(movie)):
 	try:
 		if(int(movie[x]['releasedate'][0]) >= YEAR):
 			name=movie[x]['name'][0]
-			name = name.replace(' ','%20')
+			name = name.replace('<96>','-')
+			name = name.replace('<85>','\'')
 			name = name.replace('\u2013','-')
 			name = name.replace('\u2026','-')
 			name = name.replace('\u2019','-')
+#			name = name.split('-',1)[0]
+			name = name.replace(' ','%20')
 			url = 'http://api.cinemalytics.in/v2/movie/title/?value='+name+'&auth_token=B8B0E93A2E911C893387530C0CBDDBBC'
 			print(url)
 			req=urllib.request.Request(url,headers=headers)
 			resp = str(urllib.request.urlopen(req).read())+'\n'
 			if(len(resp)>10):
-				f.write(resp)
+				print(resp)
+#				f.write(resp)
 	except:
 		print('error occured')
-f.close()
+#f.close()
